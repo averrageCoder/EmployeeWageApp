e11ca4e087a81f071c88c88d4824bb446222f316
Harsh R Jain <rjainharsh@gmail.com>
2021-10-12 05:24:14 +0530

    ADDED on submit populate the employee data object

--- js/employeePayroll.js
+++ js/employeePayroll.js
@@ -4,3 +4,21 @@ salary_output.textContent = salary.value;
 salary.addEventListener('input',function() {
     salary_output.textContent = salary.value;
 });
+
+function save() {
+    const name_val = document.querySelector('#name').value;
+    const salary_val = salary.value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const day = document.querySelector('#day').value;
+    const month = parseInt(document.querySelector('#month').value) -1;
+    const year = document.querySelector('#year').value;
+    const start_date = new Date(year,month,day);
+    console.log(start_date);
+    try {
+        let newEmployeePayrllData  = new EmployeePayrllData(1, name_val,salary_val, gender, start_date);
+        alert("saved successfully : "+ newEmployeePayrllData);
+    }
+    catch (e) {
+        alert(e);
+    }
+}
